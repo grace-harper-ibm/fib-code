40576842add0feb0f5d1c07e1d1d57292b12a9dc
Grace Harper <grace.harper@ibm.com>
2022-11-02 19:36:34 -0400

    fund symm looks fine

--- classic_fib.py
+++ classic_fib.py
@@ -1,11 +1,12 @@
 # grug brain 
 import math
 import random
+from turtle import st
 
 import numpy as np
 
 
-class fib_code():
+class FibCode():
     """
     self.board = 
     [L*(L//2 - 1)....          ((L**2)//2) - 1]
@@ -16,7 +17,7 @@ class fib_code():
      L 
      0 1 2 3 ....                                L - 1]
     """
-    def __init__(self, L=8,p=0.001, decip=1000):
+    def __init__(self, L=8,p=0.001, decip=1000, start_arr = None):
         assert math.log2(L) % 1 == 0, "L must be some 2**n where n is an int >= 1"
         self.L = L # len
         self.no_bits = (L**2)//2 # no bits
@@ -24,7 +25,8 @@ class fib_code():
         self.decip = decip
         # self.board = np.packbits(np.zeros((L**2)//2), axis=1)
         self.board =np.zeros((self.L**2)//2, dtype=int)
-        self.fund_symm = self._generate_fundamental_symmetry()
+        self.fund_symm = self._generate_fundamental_symmetry(start_arr)
+        print(self.fund_symm)
 
     def set_code_word(bottom_row_start_sequence):
         pass 
@@ -46,14 +48,14 @@ class fib_code():
     # def rc_to_bit(self, rc):
         
     def _generate_plus_x_trans_matrix(self):
-        H = np.zeros((self.n,  self.n), dtype=int)
-        for b in  range(self.n):
+        H = np.zeros((self.no_bits,  self.no_bits), dtype=int)
+        for b in  range(self.no_bits):
             #H[new_bit][old_bit]
             print(b)
             new_bit = ((b + 1) %self.L)  + ((b//self.L) * (self.L)) 
             H[new_bit][b] = 1
         return H 
-        
+
     def _generate_plus_y_trans_matrix(self):
         " performs a -1 y (or a + 1 y if you're indexing rows top to bottom like numpy :...( "
         H = np.zeros(( self.no_bits,  self.no_bits), dtype=int)
@@ -63,9 +65,21 @@ class fib_code():
             H[new_bit][b] = 1
         return H 
 
-    def _generate_fundamental_symmetry(self):
-        midpoint = self.L//2
+    def _generate_fundamental_symmetry(self, start_arr = None):
+        # fundamental symmetries start from the top instead of the bottom because numpy
+        rect_board = np.reshape(self.board, (self.L//2, self.L))
+        if not start_arr: 
+            midpoint = self.L//2
+            start_arr = np.zeros(self.L, dtype=int)
+            start_arr[midpoint] = 1
+        rect_board[0] = start_arr
+        for row in range(1, self.L//2):
+            for bit in range(self.L):
+                new_val =  rect_board[row - 1][(bit - 1)%self.L] ^  rect_board[row - 1][(bit )%self.L] ^  rect_board[row - 1][(bit + 1)%self.L]
+                rect_board[row][bit] = new_val
+        return rect_board
         # build fundamental symmetry, into array (ones on which ones)
+        
         
         # which stabs go on the fundamental symmetry? 
         
