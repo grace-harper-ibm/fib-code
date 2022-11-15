8ea9b9ae739302f6f00f196a1ca6c9658f221f74
Grace Harper <grace.harper@ibm.com>
2022-11-15 09:48:38 -0500

    black + all_stabs_check_mat looks fine by inspection

--- chill_vibes.py
+++ chill_vibes.py
@@ -3,8 +3,8 @@ import numpy as np
 from classic_fib import FibCode
 
 if __name__ == "__main__":
-   
-    # Issues: Decoder should pass if there is only a codeword 
+
+    # Issues: Decoder should pass if there is only a codeword
     L = 4
     print(L)
     f = FibCode(L, p=0.05)
